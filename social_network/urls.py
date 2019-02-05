5b65a0b4a0fe14dafb126f5662993eb59cc0ec37
ksd9 <evtimov9@gmail.com>
2019-02-05 11:06:50 +0200

    Initial commit with project setup

---
+++ social_network/urls.py
@@ -0,0 +1,12 @@
+
+
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+
+    url(r'^admin/', admin.site.urls),
+    path('', include('social.urls')),
+]
